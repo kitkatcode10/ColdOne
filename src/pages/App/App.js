1defc64688c3304dbc0264235247cbc4327cfaf4
Katherine Wong <katherinewongkw3@gmail.com>
2021-08-06 13:25:47 -0700

    fix app.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,61 +2,67 @@ import React, { Component } from "react";
 import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import SignupPage from "../SignupPage/SignupPage";
-import NavBar from "../../components/NavBar/NavBar"; 
 import HomePage from '../HomePage/HomePage'; 
 import LoginPage from '../LoginPage/LoginPage';
 import userService from '../../utils/userService'; 
+import NavBar from "../../components/NavBar/NavBar";
+import tokenService from '../../utils/tokenService';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      ...this.getInitialState(),
-      user: userService.getUser() 
+      user: userService.getUser(), 
+      brewery: "",
+      myRating: "", 
     };
   }
 
-  getInitialState() {
-    return {
-
+    handleSignupOrLogin = () => {
+      this.setState({ user: userService.getUser() }); 
     };
-  }
 
     handleLogout = () => {
       userService.logout();
-      this.setState({user: null})
-    }
-
-    handleSignupOrLogin = () =>{
-      this.setState({user: userService.getUser()}); 
-    }
+      this.setState({ user: null });
+    };
 
 
 render() {
   return (
     <div className="App">
-      <header className="App-header"> <p>Here, have a Cold One Bud! 🍺 </p></header>
+      <header className="App-header"> 
+        <NavBar
+        {...this.props}
+        user={this.state.user}
+        handleLogout={this.handleLogout} 
+        />
+        <p>Here, have a Cold One Bud! 🍺 </p></header>
+      
       <Switch> 
         <Route exact path='/signup' render={({ history }) =>   
         <SignupPage
           history={history}
+          handleSignupOrLogin={this.handleSignupOrLogin}
           />
         }/>
-        <Route exact path='/login' render={() => 
+        <Route exact path='/login' render={({ history }) => 
           <LoginPage
+          history={history}
+          handleSignupOrLogin={this.handleSignupOrLogin}
           />
         }/>
       <Route exact path='/' render={() => 
       <HomePage
         />
       }/>
-      <Route exact path='/mybeers' render={() => (
+      <Route exact path='/mybeers' render={() => 
         userService.getUser() ?
         <HomePage /> 
           :
         <Redirect to='/login' />
-      )}/>
+      }/>
 
       </Switch>
       </div>
